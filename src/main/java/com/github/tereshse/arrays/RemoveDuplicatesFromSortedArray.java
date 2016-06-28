fc4aa5cc1df63b6923c354be79e558f6dabe614a
Sergii <Tereshchenko>
2016-06-27 22:11:32 -0400

    + Add RemoveDuplicatesFromSortedArray

---
+++ src/main/java/com/github/tereshse/arrays/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,47 @@
+package com.github.tereshse.arrays;
+
+import com.google.common.base.Preconditions;
+
+
+public class RemoveDuplicatesFromSortedArray {
+    public static int bruteForceRemoveDuplicatesFromSortedArray(int[] array) {
+        Preconditions.checkArgument(array != null);
+        Preconditions.checkArgument(isSorted(array));
+        int length = array.length;
+        for (int i = 0; i < length - 1; ) {
+            if (array[i] == array[i + 1]) {
+                System.arraycopy(array, i + 1, array, i, length - i - 1);
+                length--;
+                array[length] = Integer.MIN_VALUE;
+            } else {
+                i++;
+            }
+        }
+        return length;
+    }
+
+    public static int removeDuplicatesFromSortedArray(int[] array) {
+        Preconditions.checkArgument(array != null);
+        Preconditions.checkArgument(isSorted(array));
+        int writeIndex = 1;
+        for (int i = 1; i < array.length; i++) {
+            if (array[writeIndex - 1] != array[i]) {
+                array[writeIndex++] = array[i];
+            }
+        }
+        return writeIndex;
+    }
+
+
+    public static boolean isSorted(int[] array) {
+        if (array == null || array.length < 2) {
+            return true;
+        }
+        for (int i = 1; i < array.length; i++) {
+            if (array[i - 1] > array[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
---
+++ src/test/java/com/github/tereshse/arrays/RemoveDuplicatesFromSortedArrayTest.java
@@ -0,0 +1,60 @@
+package com.github.tereshse.arrays;
+
+import org.junit.Test;
+
+import static com.github.tereshse.arrays.RemoveDuplicatesFromSortedArray.bruteForceRemoveDuplicatesFromSortedArray;
+import static com.github.tereshse.arrays.RemoveDuplicatesFromSortedArray.removeDuplicatesFromSortedArray;
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class RemoveDuplicatesFromSortedArrayTest {
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionIfArrayIsNull() {
+        bruteForceRemoveDuplicatesFromSortedArray(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void shouldThrowIllegalArgumentExceptionIfArrayIsNotSorted() {
+        bruteForceRemoveDuplicatesFromSortedArray(new int[]{1, 2, -1});
+    }
+
+
+    @Test
+    public void shouldNotModifyArrayIfThereAreNoDuplicates() {
+        int[] array = new int[]{1, 2, 3};
+        assertThat(bruteForceRemoveDuplicatesFromSortedArray(array)).isEqualTo(3);
+        assertThat(array).containsExactly(1, 2, 3);
+    }
+
+
+    @Test
+    public void shouldModifyArrayIfThereAreDuplicates() {
+        int[] array = new int[]{1, 2, 2, 3};
+        assertThat(bruteForceRemoveDuplicatesFromSortedArray(array)).isEqualTo(3);
+        assertThat(array).containsExactly(1, 2, 3, Integer.MIN_VALUE);
+
+        array = new int[]{1, 1, 2, 2, 3, 3};
+        assertThat(bruteForceRemoveDuplicatesFromSortedArray(array)).isEqualTo(3);
+        assertThat(array).containsExactly(1, 2, 3, Integer.MIN_VALUE, Integer.MIN_VALUE, Integer.MIN_VALUE);
+    }
+
+    @Test
+    public void shouldModifyArrayIfThereAreDuplicatesOptimal() {
+        int[] array = new int[]{1, 2, 2, 3};
+        assertThat(removeDuplicatesFromSortedArray(array)).isEqualTo(3);
+        assertThat(array).containsExactly(1, 2, 3, 3);
+
+        array = new int[]{1, 1, 1, 1, 2, 2, 2, 3, 3, 3};
+        assertThat(removeDuplicatesFromSortedArray(array)).isEqualTo(3);
+        assertThat(array).containsExactly(1, 2, 3, 1, 2, 2, 2, 3, 3, 3);
+    }
+
+
+    @Test
+    public void testIsSorted() {
+        assertThat(RemoveDuplicatesFromSortedArray.isSorted(null)).isTrue();
+        assertThat(RemoveDuplicatesFromSortedArray.isSorted(new int[]{1})).isTrue();
+        assertThat(RemoveDuplicatesFromSortedArray.isSorted(new int[]{1, 2})).isTrue();
+        assertThat(RemoveDuplicatesFromSortedArray.isSorted(new int[]{3, 1})).isFalse();
+    }
+}
